11d34ee9f7248a203ed09c896d7dc4eb658f79f7
Bi-Rabittoh <andronacomarco@gmail.com>
2019-04-04 16:00:01 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 javac siciliaguerrabot2020/SiciliaGuerraBot2020.java
-jar cmf MANIFEST.MF SiciliaGuerraBot2020.jar siciliaguerrabot2020
+jar cmf MANIFEST.MF SiciliaGuerraBot2020.jar siciliaguerrabot2020 picocli
 
